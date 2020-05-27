b0c6816dae5876e3077fd0846dc5199d9ea26601
rjgu <ricardojosegomezulmke@gmail.com>
2020-05-27 18:13:58 +0100

    added locked settings

--- docker/context/start.sh
+++ docker/context/start.sh
@@ -4,9 +4,18 @@
 #################################################
 # Start node RED
 #
+
+SETTINGS_FILE="solace-asapio-demo-settings-locked.js"
+
+if [ -z ${IS_LOCKED+x} ]; then
+  # IS_LOCKED is not set
+  SETTINGS_FILE="solace-asapio-demo-settings.js"
+fi
+
+#echo "SETTINGS_FILE=$SETTINGS_FILE"
+
 # size is in Mega Bytes
-#
-node --max-old-space-size=1024 /usr/local/bin/node-red -v -s solace-asapio-demo-settings.js
+node --max-old-space-size=1024 /usr/local/bin/node-red -v -s $SETTINGS_FILE
 
 
 
--- solace-asapio-demo-settings-locked.js
+++ solace-asapio-demo-settings-locked.js
@@ -94,8 +94,8 @@ module.exports = {
     // By default, the Node-RED UI is available at http://localhost:1880/
     // The following property can be used to specify a different root path.
     // If set to false, this is disabled.
-    httpAdminRoot: '/admin',
-    //httpAdminRoot: false,
+    //httpAdminRoot: '/admin',
+    httpAdminRoot: false,
 
     // Some nodes, such as HTTP In, can be used to listen for incoming http requests.
     // By default, these are served relative to '/'. The following property
